484318b15c6224f31afe16f764b8d6d3b2c5ead8
wshessler <wshessler@crimson.ua.edu>
2026-02-10 15:36:53 -0600

    Initial Commit: Forrest Gump Movie Project

---
+++ Spring2026-Project1-wshessler/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Spring2026_Project1_wshessler.Models;
+
+namespace Spring2026_Project1_wshessler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Spring2026-Project1-wshessler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project1_wshessler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
